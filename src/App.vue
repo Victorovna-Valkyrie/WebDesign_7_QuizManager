36d92056b4cd51005ba78ad6fdf4a90a27bbae0d
Victorovna-Valkyrie <1594674303@qq.com>
2025-11-07 10:50:58 +0800

    初始化项目：提交所有前端文件

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+      <!-- <user-view></user-view> -->
+      <!-- <question-view></question-view> -->
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import QuestionView from './views/element/QuestionView.vue';
+// import UserView from './views/element/UserView.vue';
+
+export default {
+  // components: { QuestionView },
+  data(){
+    return{
+      message: "hello vue",
+    };
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style>
+
+</style>
